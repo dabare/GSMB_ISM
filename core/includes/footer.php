d4fa1aacf9f3b0d9c3a4bd6346d4d0a114a6fb9f
madhava <dabaremadhava@gmail.com>
2018-08-16 21:31:22 +0530

    headers

--- core/includes/footer.php
+++ core/includes/footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
 	<div class="pull-right">
-
+		<strong>Copyright</strong> <small>COREDEPTH</small> &copy; 2017-2018
 	</div>
 	<div>
-		<strong>Copyright</strong> <small>COREDEPTH</small> &copy; 2017-2018
+		
 	</div>
 </div>
